e83159429f2853921ac216d6e216601264c164eb
Vlastimil Holer <vholer@opennebula.org>
2017-07-31 11:27:07 +0200

    Add dependencies on open-vm-tools, qemu-guest-agent

--- targets.sh
+++ targets.sh
@@ -4,7 +4,7 @@ case "${TARGET}" in
         RELSUFFIX=${RELSUFFIX:-.el6}
         TYPE=${TYPE:-rpm}
         TAGS=${TAGS:-rpm sysv one}
-        DEPENDS=${DEPENDS:-util-linux-ng bind-utils cloud-utils-growpart dracut-modules-growroot ruby rubygem-json}
+        DEPENDS=${DEPENDS:-util-linux-ng bind-utils cloud-utils-growpart dracut-modules-growroot ruby rubygem-json open-vm-tools qemu-guest-agent}
         REPLACES=${REPLACES:-cloud-init}
         POSTIN=${POSTINST:-postinstall.one}
         PREUN=${PREUN:-preuninstall.one}
@@ -27,7 +27,7 @@ case "${TARGET}" in
         RELSUFFIX=${RELSUFFIX:-.el7}
         TYPE=${TYPE:-rpm}
         TAGS=${TAGS:-rpm systemd one}
-        DEPENDS=${DEPENDS:-util-linux bind-utils cloud-utils-growpart ruby rubygem-json}
+        DEPENDS=${DEPENDS:-util-linux bind-utils cloud-utils-growpart ruby rubygem-json open-vm-tools qemu-guest-agent}
         REPLACES=${REPLACES:-cloud-init}
         POSTIN=${POSTINST:-postinstall.one}
         PREUN=${PREUN:-preuninstall.one}
@@ -49,7 +49,7 @@ case "${TARGET}" in
         RELSUFFIX=${RELSUFFIX:-.suse}
         TYPE=${TYPE:-rpm}
         TAGS=${TAGS:-rpm systemd one}
-        DEPENDS=${DEPENDS:-util-linux bind-utils growpart ruby} # rubygem-json}
+        DEPENDS=${DEPENDS:-util-linux bind-utils growpart ruby open-vm-tools qemu-guest-agent} # rubygem-json}
         REPLACES=${REPLACES:-cloud-init cloud-init-config-suse}
         POSTIN=${POSTINST:-postinstall.one}
         PREUN=${PREUN:-preuninstall.one}
@@ -60,7 +60,7 @@ case "${TARGET}" in
         RELSUFFIX=${RELSUFFIX:-}
         TYPE=${TYPE:-deb}
         TAGS=${TAGS:-deb sysv systemd upstart one}
-        DEPENDS=${DEPENDS:-util-linux bind9-host cloud-utils ruby python acpid}
+        DEPENDS=${DEPENDS:-util-linux bind9-host cloud-utils ruby python acpid open-vm-tools qemu-guest-agent}
         REPLACES=${REPLACES:-cloud-init}
         POSTIN=${POSTINST:-postinstall.one}
         PREUN=${PREUN:-preuninstall.one}
