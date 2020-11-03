9b98a6ada8841b4626a57f099306d51c3d4075e2
Vlastimil Holer <vholer@opennebula.io>
2020-11-03 17:39:03 +0100

    M #-: Bump version to 5.12.0.2

--- generate.sh
+++ generate.sh
@@ -39,7 +39,7 @@ fi
 
 ###
 
-VERSION=${VERSION:-5.12.0.1}
+VERSION=${VERSION:-5.12.0.2}
 RELEASE=${RELEASE:-0}
 MAINTAINER=${MAINTAINER:-OpenNebula Systems <support@opennebula.systems>}
 LICENSE=${LICENSE:-Apache 2.0}
